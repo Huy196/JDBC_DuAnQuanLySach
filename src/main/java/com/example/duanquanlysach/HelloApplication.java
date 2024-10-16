54bcfb0056b4b4688b3705b21bff71ed6c585a30
doquochuy <doquochuyy66@gmail.com>
2024-10-16 23:37:25 +0700

    GiaoDienLoginHoanChinh

--- src/main/java/com/example/duanquanlysach/HelloApplication.java
+++ src/main/java/com/example/duanquanlysach/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("DangNhap.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 720, 540);
+        Scene scene = new Scene(fxmlLoader.load(), 920, 740);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
