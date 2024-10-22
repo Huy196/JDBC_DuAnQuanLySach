023e4ee5c2dc59ae3871245348ba768cd6e78f93
doquochuy <doquochuyy66@gmail.com>
2024-10-22 10:45:17 +0700

    Quản lý sản phẩm Admin

---
+++ src/main/java/com/example/duanquanlysach/Product.java
@@ -0,0 +1,120 @@
+package com.example.duanquanlysach;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Product {
+    private int maSach;
+    private String Anh;
+    private String tenSach;
+    private String tacGia;
+    private String noiDung;
+    private int namXB;
+    private int maNXB;
+    private double giaSach;
+    private int soLuong;
+    private int maLoaiSach;
+    private String trangThai;
+
+    public Product(int maSach,String anh, String tenSach, String tacGia, String noiDung, int namXB, int maNXB, double giaSach, int soLuong, int maLoaiSach, String trangThai) {
+        this.maSach = maSach;
+        this.tenSach = tenSach;
+        this.tacGia = tacGia;
+        this.noiDung = noiDung;
+        this.namXB = namXB;
+        this.maNXB = maNXB;
+        this.giaSach = giaSach;
+        this.soLuong = soLuong;
+        this.maLoaiSach = maLoaiSach;
+        this.trangThai = trangThai;
+        this.Anh = anh;
+    }
+
+    public String getAnh() {
+        return Anh;
+    }
+
+    public void setAnh(String anh) {
+        Anh = anh;
+    }
+
+    public int getMaSach() {
+        return maSach;
+    }
+
+    public void setMaSach(int maSach) {
+        this.maSach = maSach;
+    }
+
+    public String getTenSach() {
+        return tenSach;
+    }
+
+    public void setTenSach(String tenSach) {
+        this.tenSach = tenSach;
+    }
+
+    public String getTacGia() {
+        return tacGia;
+    }
+
+    public void setTacGia(String tacGia) {
+        this.tacGia = tacGia;
+    }
+
+    public String getNoiDung() {
+        return noiDung;
+    }
+
+    public void setNoiDung(String noiDung) {
+        this.noiDung = noiDung;
+    }
+
+    public int getNamXB() {
+        return namXB;
+    }
+
+    public void setNamXB(int namXB) {
+        this.namXB = namXB;
+    }
+
+    public int getMaNXB() {
+        return maNXB;
+    }
+
+    public void setMaNXB(int maNXB) {
+        this.maNXB = maNXB;
+    }
+
+    public double getGiaSach() {
+        return giaSach;
+    }
+
+    public void setGiaSach(double giaSach) {
+        this.giaSach = giaSach;
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+
+    public int getMaLoaiSach() {
+        return maLoaiSach;
+    }
+
+    public void setMaLoaiSach(int maLoaiSach) {
+        this.maLoaiSach = maLoaiSach;
+    }
+
+    public String getTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(String trangThai) {
+        this.trangThai = trangThai;
+    }
+}
--- src/main/java/com/example/duanquanlysach/ProductController.java
+++ src/main/java/com/example/duanquanlysach/ProductController.java
@@ -1,48 +1,201 @@
 package com.example.duanquanlysach;
 
+import ConnectionDatabase.ConnectionDatabase;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Optional;
+import java.util.ResourceBundle;
 
-public class ProductController {
+public class ProductController implements Initializable {
     @FXML
-    private AnchorPane contentArea;
+    private VBox contentArea;
+    @FXML
+    private TableView<Product> tableView;
+    @FXML
+    private TableColumn<Product, Integer> idColumn;
 
-    public void logOut(){
-        try {
+    @FXML
+    private TableColumn<Product,String> anhColumn;
+    @FXML
+    private TableColumn<Product, String> nameBookColumn;
+    @FXML
+    private TableColumn<Product, String> authorColumn;
+    @FXML
+    private TableColumn<Product, String> contentColumn;
+    @FXML
+    private TableColumn<Product, Integer> yearXBColumn;
+    @FXML
+    private TableColumn<Product, Integer> ghostNXBColumn;
+    @FXML
+    private TableColumn<Product, Double> priceBookColumn;
+    @FXML
+    private TableColumn<Product, Integer> quantityColumn;
+    @FXML
+    private TableColumn<Product, Integer> codeBookColumn;
+    @FXML
+    private TableColumn<Product, String> statusColumn;
+    @FXML
+    private TableColumn<Product, String> functionColumn;
+    private ObservableList<Product> products = FXCollections.observableArrayList();
 
-            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-            alert.setTitle("Confirm sign-out");
-            alert.setHeaderText(null);
-            alert.setContentText("Are you sure you want to log out ?");
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        idColumn.setCellValueFactory(new PropertyValueFactory<Product,Integer>("maSach"));
+        anhColumn.setCellValueFactory(new PropertyValueFactory<Product,String>("Anh"));
+        anhColumn.setCellFactory(column -> new TableCell<Product,String>() {
+            private final ImageView imageView = new ImageView();
 
-            Optional<ButtonType> result = alert.showAndWait();
-
-            if (result.isPresent() && result.get() == ButtonType.OK) {
-
-                Main.changeScene("Login.fxml");
+            @Override
+            protected void updateItem(String imagePath, boolean empty) {
+                super.updateItem(imagePath, empty);
+                if (empty || imagePath == null || imagePath.isEmpty()) {
+                    setGraphic(null);
+                } else {
+                    Image image = new Image(imagePath, 100, 100, true, true);
+                    imageView.setFitHeight(100);
+                    imageView.setFitWidth(120);
+                    imageView.setImage(image);
+                    imageView.setPreserveRatio(true);
+                    setGraphic(imageView);
+                }
             }
+        });
+        nameBookColumn.setCellValueFactory(new PropertyValueFactory<Product,String>("tenSach"));
+        authorColumn.setCellValueFactory(new PropertyValueFactory<Product,String>("tacGia"));
+        contentColumn.setCellValueFactory(new PropertyValueFactory<Product,String>("noiDung"));
+        yearXBColumn.setCellValueFactory(new PropertyValueFactory<Product,Integer>("namXB"));
+        ghostNXBColumn.setCellValueFactory(new PropertyValueFactory<Product,Integer>("maNXB"));
+        priceBookColumn.setCellValueFactory(new PropertyValueFactory<Product,Double>("giaSach"));
+        quantityColumn.setCellValueFactory(new PropertyValueFactory<Product,Integer>("soLuong"));
+        codeBookColumn.setCellValueFactory(new PropertyValueFactory<Product,Integer>("maLoaiSach"));
+        statusColumn.setCellValueFactory(new PropertyValueFactory<Product,String>("trangThai"));
 
-        }catch (Exception e){
+        loadData();
+
+        functionColumn.setCellFactory(param -> new TableCell<>() {
+            private final Button editButton = new Button("Sửa");
+            private final Button deleteButton = new Button("Xóa");
+
+            @Override
+            protected void updateItem(String item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty) {
+                    setGraphic(null);
+                    setText(null);
+                } else {
+                    HBox hBox = new HBox(editButton, deleteButton);
+                    hBox.setSpacing(10);
+
+                    editButton.setOnAction(event -> {
+                        Product product = getTableView().getItems().get(getIndex());
+                        editProduct(product);
+                    });
+
+                    deleteButton.setOnAction(event -> {
+                        Product product = getTableView().getItems().get(getIndex());
+                        deleteProduct(product);
+                    });
+
+                    setGraphic(hBox);
+                    setText(null);
+                }
+            }
+        });
+    }
+
+    public void editProduct(Product product) {
+        System.out.println("Chỉnh sửa sản phẩm: ");
+    }
+
+    public void deleteProduct(Product product) {
+        System.out.println("Xóa sản phẩm: ");
+    }
+
+    public void loadData() {
+        try {
+            ConnectionDatabase connectionDatabase = new ConnectionDatabase();
+            connectionDatabase.connection();
+            var connection = connectionDatabase.connection();
+
+            String SQL = "SELECT * FROM sach";
+
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(SQL);
+
+
+            while (resultSet.next()) {
+                Product product = new Product(
+                        resultSet.getInt("MaSach"),
+                        resultSet.getString("Anh"),
+                        resultSet.getString("TenSach"),
+                        resultSet.getString("TacGia"),
+                        resultSet.getString("NoiDung"),
+                        resultSet.getInt("NamXuatBan"),
+                        resultSet.getInt("MaNXB"),
+                        resultSet.getDouble("GiaSach"),
+                        resultSet.getInt("SoLuong"),
+                        resultSet.getInt("MaLoaiSach"),
+                        resultSet.getString("TrangThai")
+                );
+                products.add(product);
+            }
+            tableView.setItems(products);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public void showProduct(){
+    public void logOut() {
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("ProductAdmin.fxml"));
-            Parent dashboardView = loader.load();
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Xác nhận đăng xuất");
+            alert.setHeaderText(null);
+            alert.setContentText("Bạn có chắc chắn muốn đăng xuất không?");
 
+            Optional<ButtonType> result = alert.showAndWait();
+
+            if (result.isPresent() && result.get() == ButtonType.OK) {
+                Main.changeScene("Login.fxml");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void showProduct() {
+        try {
             contentArea.getChildren().clear();
-            contentArea.getChildren().add(dashboardView);
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("ProductAdmin.fxml"));
+            Parent parent = loader.load();
+
+            contentArea.getChildren().add(parent);
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 }
+
